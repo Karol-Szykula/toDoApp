cb669f74c8d5deb8cf647a4e4e9effc2dc8987fc
Karol-Szykula <karol.szykula@wp.pl>
2018-11-18 10:26:18 +0100

    Add part empty UI of render function

--- script.js
+++ script.js
@@ -24,10 +24,10 @@ class ToDoApp {
     render() {
         //render UI
         const appDiv = document.createElement('div')
+        appDiv.innerHTML = ''
 
-        const addTaskInputField = document.createElement("INPUT");
+        const addTaskInputField = document.createElement("input");
         addTaskInputField.setAttribute("type", "text");
-
         const inputTaskButton = document.createElement('button')
         inputTaskButton.innerHTML = 'Dodaj'
 
@@ -35,6 +35,46 @@ class ToDoApp {
         inputTaskDiv.appendChild(addTaskInputField)
         inputTaskDiv.appendChild(inputTaskButton)
         appDiv.appendChild(inputTaskDiv)
+
+        const findTaskInputField = document.createElement("input");
+        findTaskInputField.setAttribute("type", "text");
+        const findTaskButton = document.createElement('button')
+        findTaskButton.innerHTML = 'Znajdź'
+
+        const findTaskDiv = document.createElement('div')
+        findTaskDiv.appendChild(findTaskInputField)
+        findTaskDiv.appendChild(findTaskButton)
+        appDiv.appendChild(findTaskDiv)
+
+        const viewOptionsDiv = document.createElement('div')
+
+        const showAllTasksButton = document.createElement('button')
+        showAllTasksButton.innerHTML = 'Wszystkie'
+
+        const showCompletedTasksButton = document.createElement('button')
+        showCompletedTasksButton.innerHTML = 'Zakończone'
+
+        const showNotCompletedTasksButton = document.createElement('button')
+        showNotCompletedTasksButton.innerHTML = 'Nie zakończone'
+
+        viewOptionsDiv.appendChild(showAllTasksButton)
+        viewOptionsDiv.appendChild(showCompletedTasksButton)
+        viewOptionsDiv.appendChild(showNotCompletedTasksButton)
+        appDiv.appendChild(viewOptionsDiv)
+
+        const listOfTaksDiv = document.createElement('div')
+        const listOfTaksHeaderDiv = document.createElement('div')
+        const listOfTaksHeader = document.createElement('h3')
+        const listOfTaksHeaderTextNode = document.createTextNode('Zadania do zrobienia:')
+
+        listOfTaksHeader.appendChild(listOfTaksHeaderTextNode)
+        listOfTaksHeaderDiv.appendChild(listOfTaksHeader)
+        listOfTaksDiv.appendChild(listOfTaksHeaderDiv)
+        listOfTaksDiv.appendChild(listOfTaksHeaderDiv)
+
+        const listOfTaksBodyDiv = document.createElement('div')
+        listOfTaksDiv.appendChild(listOfTaksBodyDiv)
+        appDiv.appendChild(listOfTaksDiv)
 
         // fill UI with data
 
