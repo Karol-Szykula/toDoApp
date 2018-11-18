8501c7e557a9269d8430cf3c33f5ec0f0e4da45e
Karol-Szykula <karol.szykula@wp.pl>
2018-11-18 13:23:54 +0100

    Remove variables from global scope, export only needed vars

--- script.js
+++ script.js
@@ -1,167 +1,168 @@
-'use strict'
+(function () {
+    class Task {
+        constructor(taskGoal) {
+            this.taskGoal = taskGoal || ''
+            this.isCompleted = false
+        }
 
-class Task {
-    constructor(taskGoal) {
-        this.taskGoal = taskGoal || ''
-        this.isCompleted = false
+        toggleCompleted() {
+            this.isCompleted = !this.isCompleted
+        }
     }
 
-    toggleCompleted() {
-        this.isCompleted = !this.isCompleted
-    }
-}
+    class ToDoApp {
+        constructor(whereToRender) {
+            this.whereToRender = whereToRender || document.body
+            this.tasks = []
 
-class ToDoApp {
-    constructor(whereToRender) {
-        this.whereToRender = whereToRender || document.body
-        this.tasks = []
+            this.init()
+        }
 
-        this.init()
-    }
+        init() {
+            this.loadDataFromLocalStorage()
 
-    init() {
-        this.loadDataFromLocalStorage()
+            this.tasks.push(new Task('kupić mleko'))
+            this.tasks.push(new Task('posprzątać'))
+            this.tasks.push(new Task('pozamiatać'))
+            this.tasks.push(new Task('pouczyć się'))
+            this.tasks.push(new Task('naprawić radio'))
+            this.tasks.push(new Task('odpocząć'))
 
-        this.tasks.push(new Task('kupić mleko'))
-        this.tasks.push(new Task('posprzątać'))
-        this.tasks.push(new Task('pozamiatać'))
-        this.tasks.push(new Task('pouczyć się'))
-        this.tasks.push(new Task('naprawić radio'))
-        this.tasks.push(new Task('odpocząć'))
+            this.render()
+        }
 
-        this.render()
-    }
+        render() {
+            //render UI
+            const appDiv = document.createElement('div')
 
-    render() {
-        //render UI
-        const appDiv = document.createElement('div')
+            const addTaskInputField = document.createElement("input");
+            addTaskInputField.setAttribute("type", "text");
+            // addTaskInputField.style.width = '100%'
+            const inputTaskButton = document.createElement('button')
+            inputTaskButton.innerHTML = 'Dodaj'
 
-        const addTaskInputField = document.createElement("input");
-        addTaskInputField.setAttribute("type", "text");
-        // addTaskInputField.style.width = '100%'
-        const inputTaskButton = document.createElement('button')
-        inputTaskButton.innerHTML = 'Dodaj'
+            inputTaskButton.addEventListener('click', this.addTask.bind(this, addTaskInputField))
 
-        inputTaskButton.addEventListener('click', this.addTask.bind(this, addTaskInputField))
+            const inputTaskDiv = document.createElement('div')
+            inputTaskDiv.appendChild(addTaskInputField)
+            inputTaskDiv.appendChild(inputTaskButton)
+            appDiv.appendChild(inputTaskDiv)
 
-        const inputTaskDiv = document.createElement('div')
-        inputTaskDiv.appendChild(addTaskInputField)
-        inputTaskDiv.appendChild(inputTaskButton)
-        appDiv.appendChild(inputTaskDiv)
+            const findTaskInputField = document.createElement("input");
+            findTaskInputField.setAttribute("type", "text");
+            const findTaskButton = document.createElement('button')
+            findTaskButton.innerHTML = 'Znajdź'
 
-        const findTaskInputField = document.createElement("input");
-        findTaskInputField.setAttribute("type", "text");
-        const findTaskButton = document.createElement('button')
-        findTaskButton.innerHTML = 'Znajdź'
+            const findTaskDiv = document.createElement('div')
+            findTaskDiv.appendChild(findTaskInputField)
+            findTaskDiv.appendChild(findTaskButton)
+            appDiv.appendChild(findTaskDiv)
 
-        const findTaskDiv = document.createElement('div')
-        findTaskDiv.appendChild(findTaskInputField)
-        findTaskDiv.appendChild(findTaskButton)
-        appDiv.appendChild(findTaskDiv)
+            const viewOptionsDiv = document.createElement('div')
 
-        const viewOptionsDiv = document.createElement('div')
+            const showAllTasksButton = document.createElement('button')
+            showAllTasksButton.innerHTML = 'Wszystkie'
 
-        const showAllTasksButton = document.createElement('button')
-        showAllTasksButton.innerHTML = 'Wszystkie'
+            const showCompletedTasksButton = document.createElement('button')
+            showCompletedTasksButton.innerHTML = 'Zakończone'
 
-        const showCompletedTasksButton = document.createElement('button')
-        showCompletedTasksButton.innerHTML = 'Zakończone'
+            const showNotCompletedTasksButton = document.createElement('button')
+            showNotCompletedTasksButton.innerHTML = 'Nie zakończone'
 
-        const showNotCompletedTasksButton = document.createElement('button')
-        showNotCompletedTasksButton.innerHTML = 'Nie zakończone'
+            viewOptionsDiv.appendChild(showAllTasksButton)
+            viewOptionsDiv.appendChild(showCompletedTasksButton)
+            viewOptionsDiv.appendChild(showNotCompletedTasksButton)
+            appDiv.appendChild(viewOptionsDiv)
 
-        viewOptionsDiv.appendChild(showAllTasksButton)
-        viewOptionsDiv.appendChild(showCompletedTasksButton)
-        viewOptionsDiv.appendChild(showNotCompletedTasksButton)
-        appDiv.appendChild(viewOptionsDiv)
+            const listOfTaksDiv = document.createElement('div')
+            const listOfTaksHeaderDiv = document.createElement('div')
+            const listOfTaksHeader = document.createElement('h3')
+            const listOfTaksHeaderTextNode = document.createTextNode('Zadania do zrobienia:')
 
-        const listOfTaksDiv = document.createElement('div')
-        const listOfTaksHeaderDiv = document.createElement('div')
-        const listOfTaksHeader = document.createElement('h3')
-        const listOfTaksHeaderTextNode = document.createTextNode('Zadania do zrobienia:')
+            listOfTaksHeader.appendChild(listOfTaksHeaderTextNode)
+            listOfTaksHeaderDiv.appendChild(listOfTaksHeader)
+            listOfTaksDiv.appendChild(listOfTaksHeaderDiv)
+            listOfTaksDiv.appendChild(listOfTaksHeaderDiv)
 
-        listOfTaksHeader.appendChild(listOfTaksHeaderTextNode)
-        listOfTaksHeaderDiv.appendChild(listOfTaksHeader)
-        listOfTaksDiv.appendChild(listOfTaksHeaderDiv)
-        listOfTaksDiv.appendChild(listOfTaksHeaderDiv)
+            const listOfTaksBodyDiv = document.createElement('div')
+            listOfTaksDiv.appendChild(listOfTaksBodyDiv)
+            appDiv.appendChild(listOfTaksDiv)
 
-        const listOfTaksBodyDiv = document.createElement('div')
-        listOfTaksDiv.appendChild(listOfTaksBodyDiv)
-        appDiv.appendChild(listOfTaksDiv)
+            // fill UI with data
+            const orderedList = document.createElement('ol')
+            this.tasks.forEach((element, taskIndex) => {
+                const listElement = document.createElement('li')
+                listElement.innerText = `${element.taskGoal}  `
 
-        // fill UI with data
-        const orderedList = document.createElement('ol')
-        this.tasks.forEach((element, taskIndex) => {
-            const listElement = document.createElement('li')
-            listElement.innerText = `${element.taskGoal}  `
+                if (element.isCompleted === true) {
+                    listElement.style.textDecoration = 'line-through'
+                }
+                else {
+                    listElement.style.textDecoration = 'none'
+                }
 
-            if (element.isCompleted === true) {
-                listElement.style.textDecoration = 'line-through'
-            }
-            else {
-                listElement.style.textDecoration = 'none'
-            }
+                listElement.addEventListener('click', (event) => {
+                    event.stopPropagation()
 
-            listElement.addEventListener('click', (event) => {
-                event.stopPropagation()
+                    element.toggleCompleted()
 
-                element.toggleCompleted()
+                    this.render()
+                })
 
-                this.render()
+                const deleteTaskButton = document.createElement('button')
+                deleteTaskButton.innerHTML = 'Usuń'
+                deleteTaskButton.addEventListener('click', this.removeTask.bind(this, taskIndex))
+                listElement.appendChild(deleteTaskButton)
+
+                orderedList.appendChild(listElement)
             })
-
-            const deleteTaskButton = document.createElement('button')
-            deleteTaskButton.innerHTML = 'Usuń'
-            deleteTaskButton.addEventListener('click', this.removeTask.bind(this, taskIndex))
-            listElement.appendChild(deleteTaskButton)
-
-            orderedList.appendChild(listElement)
-        })
-        listOfTaksBodyDiv.appendChild(orderedList)
+            listOfTaksBodyDiv.appendChild(orderedList)
 
 
-        this.whereToRender.innerHTML = ''
-        this.whereToRender.appendChild(appDiv)
-    }
+            this.whereToRender.innerHTML = ''
+            this.whereToRender.appendChild(appDiv)
+        }
 
-    addTask(addTaskInputField) {
-        this.tasks.push(new Task(addTaskInputField.value))
+        addTask(addTaskInputField) {
+            this.tasks.push(new Task(addTaskInputField.value))
 
-        this.saveDataToLocalStorage()
-        this.render()
-    }
+            this.saveDataToLocalStorage()
+            this.render()
+        }
 
-    removeTask(taskIndex) {
-        this.tasks.splice(taskIndex, 1);
-        this.saveDataToLocalStorage()
+        removeTask(taskIndex) {
+            this.tasks.splice(taskIndex, 1);
+            this.saveDataToLocalStorage()
 
-        this.render()
-    }
+            this.render()
+        }
 
-    findTask() {
+        findTask() {
 
-    }
+        }
 
-    saveDataToLocalStorage() {
-        localStorage.setItem('tasks', JSON.stringify(this.tasks));
-    }
+        saveDataToLocalStorage() {
+            localStorage.setItem('tasks', JSON.stringify(this.tasks));
+        }
 
-    loadDataFromLocalStorage() {
-        this.tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
-    }
+        loadDataFromLocalStorage() {
+            this.tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+        }
 
-    showAllTasks() {
+        showAllTasks() {
 
-    }
+        }
 
-    showCompletedTasks() {
+        showCompletedTasks() {
+
+        }
+
+        showNotCompletedTasks() {
+
+        }
 
     }
 
-    showNotCompletedTasks() {
-
-    }
-
-}
-
+    window.ToDoApp = ToDoApp
+}())
 const toDoApp = new ToDoApp()
